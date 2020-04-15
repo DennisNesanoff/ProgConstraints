baca5e554d7490a6fcdd83a6dca8cb4413890ee7
Dennis <nndenninn@gmail.com>
2020-04-15 21:44:25 +0300

    Added two views

--- ProgConstraints/ViewController.swift
+++ ProgConstraints/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
 
     lazy var blueView: UIView = {
         let _blueView = UIView()
-        _blueView.backgroundColor = UIColor.red
+        _blueView.backgroundColor = UIColor.blue
         _blueView.translatesAutoresizingMaskIntoConstraints = false
         return _blueView
     }()
@@ -35,8 +35,17 @@ class ViewController: UIViewController {
         view.addSubview(blueView)
         
         var constraints: [NSLayoutConstraint] = [NSLayoutConstraint]()
-        constraints.append(redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0))
+        constraints.append(redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0))
         constraints.append(redView.heightAnchor.constraint(equalToConstant: 120))
+        constraints.append(redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0))
+        constraints.append(redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0))
+        
+        constraints.append(NSLayoutConstraint(item: blueView, attribute: .top, relatedBy: .equal, toItem: redView, attribute: .bottom, multiplier: 1.0, constant: 20))
+        constraints.append(NSLayoutConstraint(item: blueView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0))
+        constraints.append(NSLayoutConstraint(item: blueView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0))
+        constraints.append(NSLayoutConstraint(item: blueView, attribute: .height, relatedBy: .equal, toItem: redView, attribute: .height, multiplier: 1.0, constant: 0))
+        
+        NSLayoutConstraint.activate(constraints)
     }
 
 }
